916afaed1f70123c741565fe0b15a4f6174f8e62
Art-OF-Work <yassirboukhima.00@gmail.com>
2023-07-13 09:16:05 -0700

    puts task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ *main - start function
+ *
+ *function - puts
+ *
+ *return: 0
+ */
+int main(void) {
+
+    puts("\"Programming is like building a multilingual puzzle\n");
+
+    return 0;
+}
