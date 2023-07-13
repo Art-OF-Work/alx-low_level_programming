d6659d8a3f1e4f6819be5165c3dbcfc47ac01a81
Art-OF-Work <135357812+Art-OF-Work@users.noreply.github.com>
2023-07-13 17:50:25 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
- *main - start function    
+ * main - Entry point
  *
- *return: 0
+ * Return: 0
  */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art\n");
-    return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
