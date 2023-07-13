00b4119c9958ccf79851aa959ef3d23bd55679a0
Art-OF-Work <yassirboukhima.00@gmail.com>
2023-07-13 09:44:34 -0700

    update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-/*
+/**
  *main - start function
  *
  *function - printf
  *
  *return: 0
  */
-int main(void) {
-
+int main(void)
+{
     printf("with proper grammar, but the outcome is a piece of art\n");
-
     return (0);
 }
