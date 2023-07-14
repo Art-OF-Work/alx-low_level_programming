d29ccd645c89c91460b5a1796890e486ed67fe33
Art-OF-Work <135357812+Art-OF-Work@users.noreply.github.com>
2023-07-14 16:07:58 +0100

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int C;
+
+	for (C = 0; C < 10; C++)
+			putchar(C);
+
+	putchar('\n'); /* Add a newline character after printing the alphabets */
+
+	return (0);
+}
