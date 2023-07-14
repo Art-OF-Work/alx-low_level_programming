ca5f0430e20b092371f36051a00f42986cab9feb
Art-OF-Work <135357812+Art-OF-Work@users.noreply.github.com>
2023-07-14 16:20:47 +0100

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char C;
+
+	for (C = 'z'; C >= 'a'; C--)
+	{
+		putchar(C);
+	}
+
+	putchar('\n'); /* Add a newline character after printing the alphabets */
+
+	return (0);
+}
