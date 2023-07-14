869b459a6473fb5011312618628fbb15b574540b
Art-OF-Work <135357812+Art-OF-Work@users.noreply.github.com>
2023-07-14 16:23:45 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,8 +9,12 @@ int main(void)
 {
 	int C;
 
-	for (C = 0; C < 16; C++)
-		putchar('0' + C);
+	for (C = 0; C < 16; C++) {
+		if (C < 10)
+			putchar('0' + C);
+		else
+			putchar('A' + C - 10);
+	}
 
 	putchar('\n'); /* Add a newline character after printing the numbers */
 
