33c1f893a62d37afd087580122c91a5ad6b09558
Art-OF-Work <135357812+Art-OF-Work@users.noreply.github.com>
2023-07-14 16:33:27 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,8 +12,11 @@ int main(void)
 	for (C = 0; C < 10; C++)
 	{
 		putchar('0' + C);
-		putchar(',');
-		putchar(' ');
+		if (C < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n'); /* Add a newline character after printing the numbers */
 
